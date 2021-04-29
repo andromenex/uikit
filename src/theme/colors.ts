aff479b836a088d35dbb7500cb15e2edcd8ddfe6
astonbullock <35285467+astonbullock@users.noreply.github.com>
2021-04-29 11:41:44 +0930

    2

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -17,7 +17,7 @@ export const brandColors = {
 export const lightColors: Colors = {
   ...baseColors,
   ...brandColors,
-  background: "#ffffff",
+  background: "#000000",
   backgroundDisabled: "#E9EAEB",
   contrast: "#191326",
   invertedContrast: "#FFFFFF",
