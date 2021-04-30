7687f93593890dd740b494b7d78e1acedd580453
astonbullock <35285467+astonbullock@users.noreply.github.com>
2021-04-30 17:49:24 +0930

    .

--- src/widgets/Menu/Menu.tsx
+++ src/widgets/Menu/Menu.tsx
@@ -127,6 +127,15 @@ const Menu: React.FC<NavProps> = ({
         />
         {/* <img src="../../images/testLogo.svg" alt="Logo" style={{width: "24px"}}/> */}
         <ul style={{listStyleType: "none", float: "left"}}>
+          <li><svg height="50" width="50">
+          <path
+              fill="none" stroke="black" strokeWidth="7"
+              d="M 45.90,12.50
+                  C 45.90,12.50 45.90,37.60 45.90,37.60
+                  45.90,37.60 4.10,37.60 4.10,37.60
+                  4.10,37.60 4.10,12.50 4.10,12.50
+                  4.10,12.50 45.90,12.50 45.90,12.50 Z" />
+          </svg></li>
           <li style={{display: "inline-block", paddingLeft: "15px", paddingRight: "15px"}}><a style={{textDecoration: "none"}} href="/swap">Swap</a></li>
           <li style={{display: "inline-block", paddingLeft: "15px", paddingRight: "15px"}}><a style={{textDecoration: "none"}} href="/#/farm">Farms</a></li>
           <li style={{display: "inline-block", paddingLeft: "15px", paddingRight: "15px"}}><a style={{textDecoration: "none"}} href="/#/pools">Pools</a></li>
